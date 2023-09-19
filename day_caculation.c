677f88c55576b91e9cc96ef68464d97d39024881
luxzhuo <luxzhuo@126.com>
2023-09-19 21:44:40 +0800

    second commit

--- day_caculation.c
+++ day_caculation.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 int main()
 {
+    int days_leap[12],days_normal[12], d1, d2, i;
+    days_leap ={30,29,31,30,31,30,31,31,30,31,30,31};
+    days_normal={30,28,31,30,31,30,31,31,30,31,30,31};
     printf("\n\nEnter 3 integers representing year,month and day seperated with space: ");
     int year,month,day;
     // year=0
@@ -9,8 +12,8 @@ int main()
     scanf("%d %d %d",&year,&month,&day);
     if (year % 4 == 0 && year % 100 !=0)
     {
-        int days_leap[12], d1, d2, i;
-        days_leap =[30,29,31,30,31,30,31,31,30,31,30,31];
+        // int days_leap[12], d1, d2, i;
+        // days_leap ={30,29,31,30,31,30,31,31,30,31,30,31};
         // m = days[month-1]
         d1=0;
         for (i=1;i<13;i++)
@@ -21,8 +24,8 @@ int main()
     }
     if (year % 400 == 0)
     {
-        int days_leap[12], d1, d2, i;
-        days_leap ={30,29,31,30,31,30,31,31,30,31,30,31};
+        // int days_leap[12], d1, d2, i;
+        // days_leap ={30,29,31,30,31,30,31,31,30,31,30,31};
         // m = days[month-1]
         d1=0;
         for (i=1;i<13;i++)
@@ -33,8 +36,8 @@ int main()
     }
     else
     {
-        int days_normal[12], d1, d2, i;
-        days_normal={30,28,31,30,31,30,31,31,30,31,30,31};
+        // int days_normal[12], d1, d2, i;
+        // days_normal={30,28,31,30,31,30,31,31,30,31,30,31};
         // m = days[month-1]
         d1=0;
         for (i=1;i<13;i++)
@@ -44,4 +47,5 @@ int main()
         d2=d1+day;
     }
     printf("%d",d2);
+    return 0;
 }

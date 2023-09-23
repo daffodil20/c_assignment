95f1e7995ea6001e233ee7e54e250b7b8b4b1c91
luxzhuo <luxzhuo@126.com>
2023-09-23 19:30:27 +0800

    include a particular case

--- calculator.c
+++ calculator.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main()
 {
-int i1,i2,sum,product,substraction_result,quotient,remainder;
-scanf("%d %d",i1,i2);
-sum = i1+i2;
-product = i1*i2;
-substraction_result = i1-i2;
-quotient = i1/i2;
-remainder = i1%i2;
-
+    int i1,i2,sum,product,substraction_result,quotient,remainder;
+    scanf("%d %d",&i1,&i2);
+    sum = i1+i2;
+    product = i1*i2;
+    substraction_result = i1-i2;
+    quotient = i1/i2;
+    remainder = i1%i2;
+    printf("%d %d %d %d %d",sum,product,substraction_result,quotient,remainder);
 }
--- time_adder.c
+++ time_adder.c
@@ -4,19 +4,19 @@ int main()
     int time1,time2;
     int t1,t2,t3,t4,temp1,temp2,total;
     printf("Please enter two time points:\n");
-    scanf("%d\n%d",&time1,&time2);
+    scanf("%4d\n%4d",&time1,&time2);
     t1 = time1 % 100;
     t2 = time2 % 100;
-    t3 = time1-t1;
-    t4 = time2-t2;
+    t3 = (time1-t1)/100;
+    t4 = (time2-t2)/100;
     temp1 = t1+t2;
     temp2 = t3+t4;
     if (temp1 >= 60)
     {
         temp1 = temp1-60;
-        temp2 = temp2+100;
+        temp2 = temp2+1;
     }
-    total = temp1+temp2%2400;
-    printf("%d",total);
+    // total = temp1+temp2%2400;
+    printf("%d%2d",temp2%24,temp1);
     return 0;
 }

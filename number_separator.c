2547b36e1d3c0c4902cf45d275219d55d71845d9
luxzhuo <luxzhuo@126.com>
2023-09-24 11:45:16 +0800

    possibly final commit:delete the if statement

--- number_separator.c
+++ number_separator.c
@@ -1,43 +1,19 @@
 #include <stdio.h>
-// #include <math.h>
 int main()
 {
-    int num,i,temp,digit,t;
+    int num,i,digit,t;
     int a[5];
-    // double i,temp;
     printf("请输入一个五位数：");
     scanf("%5d",&num);
     for (i = 4;i >= 0;i--)
     {
-        if (i==4)
-        {
-            digit = num%10;
-            temp = num/10;
-            a[i] = digit;
-            // display = (temp-display)/10;
-            // printf("%d ",a[i]);
-        }
-        if (i<4)
-        {
-            digit = temp%10;
-            temp = temp/10;
-            a[i] = digit;
-            // display = (temp-display)/10;
-            // printf("%d ",a[i]);
-        }
-    
+        digit = num%10;
+        num = num/10;
+        a[i] = digit;
     }
     for (t=0;t<5;t++)
     {
         printf("%d ",a[t]);
     }
-        // temp = 1.0*(num/pow(10.0,i))%10;
-        
-        // num2 = (num/1000)%10;
-        // num3 = (num/100)%10;
-        // num4 = (num/10)%10;
-        // num5 = num%10;
-    // printf("%d %d %d %d %d",num1,num2,num3,num4,num5);
     return 0;
-
 }

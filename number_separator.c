1e496d9f34c737c287e961be02661aa368a10b5f
luxzhuo <luxzhuo@126.com>
2023-09-24 09:39:06 +0800

    second commit:an easier method

--- number_separator.c
+++ number_separator.c
@@ -4,12 +4,12 @@ int main()
     int num,num1,num2,num3,num4,num5;
     printf("请输入一个五位数：");
     scanf("%5d",&num);
-    num1 = num%10;
-    num2 = ((num-num1)%100)/10;
-    num3 = ((num-num1-num2*10)%1000)/100;
-    num4 = ((num-num1-num2*10-num3*100)%10000)/1000;
-    num5 = ((num-num1-num2*10-num3*100-num4*1000)%100000)/10000;
-    printf("%d %d %d %d %d",num5,num4,num3,num2,num1);
+    num1 = num/10000;
+    num2 = (num/1000)%10;
+    num3 = (num/100)%10;
+    num4 = (num/10)%10;
+    num5 = num%10;
+    printf("%d %d %d %d %d",num1,num2,num3,num4,num5);
     return 0;
 
 }

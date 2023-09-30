dceb940a18634e25c3e9a81ed90407aa0c86844f
luxzhuo <luxzhuo@126.com>
2023-09-30 15:05:53 +0800

    replace d1,d2 with total

--- day_calculation.c
+++ day_calculation.c
@@ -1,40 +1,30 @@
 #include <stdio.h>
-int main()
-{
-    int d1, d2;
+int main(){
+    int total;
     int year,month,day;
     int days_leap[12] ={31,29,31,30,31,30,31,31,30,31,30,31};
     int days_normal[12]={31,28,31,30,31,30,31,31,30,31,30,31};
     printf("\n\nEnter 3 integers representing year,month and day seperated with space: ");
     scanf("%d %d %d",&year,&month,&day);
-    d1=0;
-    if (year % 4 == 0 && year % 100 !=0)
-    {
-        while (month-2>=0)
-        {
-            d1 = d1 + days_leap[month-2];
+    total = 0;
+    if (year % 4 == 0 && year % 100 !=0){
+        while (month-2>=0){
+            total = total + days_leap[month-2];
             month--;
         }
-        d2 = d1 + day;
     }
-    if (year % 400 == 0)
-    {
-        while (month-2>=0)
-        {
-            d1 = d1 + days_leap[month-2];
+    if (year % 400 == 0){
+        while (month-2>=0){
+            total = total + days_leap[month-2];
             month--;
         }
-        d2 = d1 + day;
-    }
-    else
-    {
-        while (month-2>=0)
-        {
-            d1 = d1 + days_normal[month-2];
+    }else{
+        while (month-2>=0){
+            total = total + days_normal[month-2];
             month--;
         }
-        d2 = d1 + day;
     }
-    printf("%d",d2);
+    total=total+day;
+    printf("%d",total);
     return 0;
 }

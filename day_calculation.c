00453423454da2d81b0cc0162178ee676a65efc7
luxzhuo <luxzhuo@126.com>
2023-09-30 14:59:14 +0800

    replace for with while loop, replace i with month

--- day_calculation.c
+++ day_calculation.c
@@ -1,50 +1,39 @@
 #include <stdio.h>
 int main()
 {
-    int d1, d2, i;
-    int days_leap[12] ={30,29,31,30,31,30,31,31,30,31,30,31};
-    int days_normal[12]={30,28,31,30,31,30,31,31,30,31,30,31};
-    printf("\n\nEnter 3 integers representing year,month and day seperated with space: ");
+    int d1, d2;
     int year,month,day;
-    // year=0
-    // month=0
-    // day=0
+    int days_leap[12] ={31,29,31,30,31,30,31,31,30,31,30,31};
+    int days_normal[12]={31,28,31,30,31,30,31,31,30,31,30,31};
+    printf("\n\nEnter 3 integers representing year,month and day seperated with space: ");
     scanf("%d %d %d",&year,&month,&day);
+    d1=0;
     if (year % 4 == 0 && year % 100 !=0)
     {
-        // int days_leap[12], d1, d2, i;
-        // days_leap ={30,29,31,30,31,30,31,31,30,31,30,31};
-        // m = days[month-1]
-        d1=0;
-        for (i=1;i<13;i++)
+        while (month-2>=0)
         {
-            d1=d1+days_leap[i-1];
+            d1 = d1 + days_leap[month-2];
+            month--;
         }
-        d2=d1+day;
+        d2 = d1 + day;
     }
     if (year % 400 == 0)
     {
-        // int days_leap[12], d1, d2, i;
-        // days_leap ={30,29,31,30,31,30,31,31,30,31,30,31};
-        // m = days[month-1]
-        d1=0;
-        for (i=1;i<13;i++)
+        while (month-2>=0)
         {
-            d1=d1+days_leap[i-1];
+            d1 = d1 + days_leap[month-2];
+            month--;
         }
-        d2=d1+day;
+        d2 = d1 + day;
     }
     else
     {
-        // int days_normal[12], d1, d2, i;
-        // days_normal={30,28,31,30,31,30,31,31,30,31,30,31};
-        // m = days[month-1]
-        d1=0;
-        for (i=1;i<13;i++)
+        while (month-2>=0)
         {
-            d1=d1+days_normal[i-1];
+            d1 = d1 + days_normal[month-2];
+            month--;
         }
-        d2=d1+day;
+        d2 = d1 + day;
     }
     printf("%d",d2);
     return 0;

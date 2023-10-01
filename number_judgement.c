050760887013c93b0bba86bdc8a122fc931acb77
luxzhuo <luxzhuo@126.com>
2023-10-01 10:45:08 +0800

    include an error case

--- number_judgement.c
+++ number_judgement.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-int main()
-{
+int main(){
     int num,digit1,digit2,digit4,digit5;
     printf("Please enter a 5 digits number:");
     scanf("%5d",&num);
@@ -8,11 +7,19 @@ int main()
     digit2 = (num%100)/10;
     digit4 = (num/1000)%10;
     digit5 = (num/1000)/10;
-    if (digit1 == digit5 && digit2 == digit4){
-        printf("YES");
+    if (num/10000 != 0){
+        if (digit1 == digit5 && digit2 == digit4){
+            printf("YES");
+        }
+        if (digit1 != digit5 || digit2 != digit4){
+            printf("NO");
+        }
     }
-    else{
-        printf("NO");
+    if (num/10000 == 0){
+        printf("error");
     }
+    // if (digit1 != digit5 || digit2 != digit4){
+    //     printf("NO");
+    // }
     return 0;
 }

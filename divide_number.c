b12b381f586bfe4adcbec59e3c629b9b16e68ded
luxzhuo <luxzhuo@126.com>
2023-10-04 12:02:46 +0800

    successfully use array to store digits of number and make the order of printing reverse

--- divide_number.c
+++ divide_number.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(){
-    int num,digits,unit=1,position,temp,result;
+    int num,digits,unit=1,position,temp,result,location;
     printf("Please enter an integer:");
     scanf("%d",&num);
     for (digits = 1;digits >= 0;digits++){
@@ -22,22 +22,43 @@ int main(){
     //     }
     // printf("%d",digits);
     // }
+    int array1[digits],array2[digits];
     unit = 10;
+    location = digits-1;
     for (position = 1;position <= digits;position++){
         if (position == 1){
-            printf("%d ",num%unit);
+            // printf("%d ",num%unit);
+            array2[position-1] = num%unit;
+            array1[digits-1] = num%unit;
         }
         if (position > 1){
             temp = num/unit;
             result = temp%10;
-            if (position == digits){
-                printf("%d\n",result);
-            }
             if (position < digits){
-                printf("%d ",result);
+                // printf("%d ",result);
+                location = location - 1;
+                array2[position-1] = result;
+                array1[location] = result;
+            }
+            if (position == digits){
+                // printf("%d\n",result);
+                location = location - 1;
+                array2[position-1] = result;
+                array1[location] = result;
             }
             unit = unit*10;
         }
+    }
+    for (position = 0;position<digits;position++){
+        if (position < digits-1){
+            printf("%d ",array1[position]);
+        }
+        if (position == digits-1){
+            printf("%d\n",array1[position]);
+        }
+    }
+    for (position = 0;position<digits;position++){
+        printf("%d ",array2[position]);
     }
 return 0;
 }

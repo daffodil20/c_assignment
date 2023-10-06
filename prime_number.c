5f0bf9accc35fec99ae23ce60bf29360cdb44bcd
luxzhuo <luxzhuo@126.com>
2023-10-06 10:18:33 +0800

    there are some problems with two counters

--- prime_number.c
+++ prime_number.c
@@ -1,37 +1,44 @@
 #include <stdio.h>
 int main(){
-    int m,n,t;
+    int m,n,indivisible_counter,composite_counter;
     printf("Please enter 2 different integers m & n(m<n):");
     scanf("%d %d",&m,&n);
-    t = 0;
+    indivisible_counter = 0;
+    composite_counter = 0;
     // a = m;
-    while (m<n){
+    while (m <= n){
         if (m == 1){
             m = m+1;
         }
-        for (int i=2;i<=m;i++){
+        int i;
+        for (i = 2;i < m;i++){
             if (m == 2){
                 printf("\nP,%d ",m);
                 m = m+1;
                 break;
             }
             if (m%i != 0){
-                if (m-i>1){
-                    t = t+1;
+                indivisible_counter = indivisible_counter + 1;
+                if (m-i > 1){
+                    // indivisible_counter = indivisible_counter + 1;
                     continue;
                 }
                 if (m-i == 1){
-                    if (t == i-2){
+                    if (indivisible_counter == m-2){
                         printf("\nP,%d ",m);
                     }
-                    m++;
-                    t=0;
+                    m = m+1;
+                    indivisible_counter = 0;
                     break;
                 }
             }
-            if (m%i == 0){
-                m++;
-                t=0;
+            if (m%i == 0 && m != 2){
+                composite_counter = composite_counter + 1;
+                if (n == m && composite_counter == m-n+1){
+                    printf("no");
+                }
+                m = m+1;
+                indivisible_counter = 0;
                 break;
             }
         }
--- primes.c
+++ primes.c
@@ -1,28 +1,33 @@
 #include <stdio.h>
 int main(){
-    int composite,prime_factor,i,factor_counter,array[100],length;
+    int composite,prime_factor,i,factor_counter,array[100],length,t;
     printf("Please input a composite number:");
     scanf("%d",&composite);
     factor_counter = 0;
     length = 0;
-    for (i = 2;i < composite; i++){
-        if (composite % i == 0){
-            composite = composite/i;
-            prime_factor = 2;
-            factor_counter ++;
+    prime_factor = 2;
+    // for (i = 2;i < composite; i++){
+    //     if (composite % i == 0){
+    //         composite = composite/i;
+    //         prime_factor = 2;
+    //         factor_counter ++;
             while (prime_factor < composite){
                 composite = composite/prime_factor;
-                if (prime_factor == i){
-                    factor_counter ++;
-                    if (factor_counter >= 2){
-                        length ++;
-                        array[length - 1] = prime_factor;
-                    prime_factor = 2;
-                    continue;
+                t = t+1;
+                if (t > 1){
+                    i = prime_factor;
+                    if (prime_factor == i){
+                        factor_counter ++;
+                        if (factor_counter >= 2){
+                            length ++;
+                            array[length - 1] = prime_factor;
+                        prime_factor = 2;
+                        continue;
+                        }
+                    }else{
+                        continue;
                     }
-                }else{
-                    continue;
-                }
+                    prime_factor = prime_factor + 1;
                 // prime_factor = prime_factor + 1;
             }
             

a05a2cd74c28813a46afb7d8eeefa14031d6e5f4
luxzhuo <luxzhuo@126.com>
2023-10-14 11:31:59 +0800

    solve the negative result problem,but print too much numbers

--- profit_deduct.c
+++ profit_deduct.c
@@ -1,30 +1,37 @@
 #include<stdio.h>
 int main(){
-    int profit;
-    double bonus;
+    // int profit;
+    double bonus,profit;
+    // float temp;
     printf("Please enter a monthly profit:");
-    scanf("%d",&profit);
+    scanf("%le",&profit);
     profit = 0.0001*profit;
-    printf("%d",profit);
+    // printf("%le",profit);
     if (profit <= 10){
         bonus = 0.1*profit;
+        printf("%.2f",bonus*10000);
     }
     if (10 < profit < 20){
-        bonus = 0.075*(profit-10);
+        bonus = 0.1*10+0.075*(profit-10);
+        printf("%.2f",bonus*10000);
     }
     if (20 <= profit <= 40){
-        bonus = 0.05*(profit-20);
+        bonus = 0.1*10+0.075*10+0.05*(profit-20);
+        printf("%.2f",bonus*10000);
     }
     if (40 < profit <= 60){
-        bonus = 0.03*(profit-40);
+        bonus = 0.1*10+0.075*10+0.05*20+0.03*(profit-40);
+        printf("%.2f",bonus*10000);
     }
     if (60 < profit <= 100){
-        bonus = 0.015*(profit-60);
+        bonus = 0.1*10+0.075*10+0.05*20+0.03*20+0.015*(profit-60);
+        printf("%.2f",bonus*10000);
     }
     if (profit > 100){
-        bonus = 0.01*(profit-100);
+        bonus = 0.1*10+0.075*10+0.05*20+0.03*20+0.015*40+0.01*(profit-100);
+        printf("%.2f",bonus*10000);
     }
-    bonus = bonus*10000;
-    printf("%.2f",bonus);
+    // bonus = bonus*10000;
+    // printf("%.2f",bonus);
     return 0;
 }
